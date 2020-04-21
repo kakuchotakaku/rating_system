37d42519f22c6d993758758c7307c435a212dd4e
kakucho takaku <miracle-spitzex@hotmail.co.jp>
2020-04-22 04:55:19 +0900

    heroku error対応

--- Gemfile
+++ Gemfile
@@ -55,9 +55,6 @@ gem 'kaminari'
 gem 'slim-rails'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: %i[mri mingw x64_mingw]
-
   # Note: manual install gems
   gem 'annotate'
   gem 'autodoc'
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module RatingSystem
   class Application < Rails::Application
